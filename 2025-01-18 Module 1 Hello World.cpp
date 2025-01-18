e9cb54c4923eb72b934ab059eae0259557a9e46d
Drouge7414 <108477680+Drouge7414@users.noreply.github.com>
2025-01-18 18:00:51 -0500

    Add files via upload

---
+++ 2025-01-18 Module 1 Hello World.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Include the input/output stream library
+using namespace std; // This line declares that I am using the name space std so that when I use syntax within the std name space I dont have to put std:: before them
+
+int main() //This is the main fuction of the and defines where the programs starts executing and ensures compatability with operating systems that look for the maian fuction to staart a program
+{
+    cout << "Hello World!\nMy name is Dylan Barrett" << endl; //This line Prints Hello World My name is Dylan with a line break 
+    return 0; //Return 0 to indicate that the program has ended succesfully
+}
